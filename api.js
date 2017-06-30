77ee56bf9d38fb611fb0585a56929a975343caea
Alistair Willis <alistairrwillis@gmail.com>
2017-06-30 16:14:34 +0900

    trial api request parameters

---
+++ api.js
@@ -0,0 +1,9 @@
+
+
+let key = require('./config.json').QUANDL_API_KEY;
+
+let altUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json';
+
+let url = `https://www.quandl.com/api/v3/datasets/LSE/IWG.json?api_key=${key}`;
+
+
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const request = require('request');
+const chalk = require('chalk');
+const app = express(); 
+// app.set('json spaces', 2);
+
+let key = require('./config.json').QUANDL_API_KEY;
+let database_code = 'LSE';
+let dataset_code = 'RBS';
+let rows = 10; 
+let interval = ['none', 'daily', 'weekly'][2];
+let column = 1; 
+// Daily prices for specific company over last 10 years
+let url = `https://www.quandl.com/api/v3/datasets/${database_code}/${dataset_code}.json?api_key=${key}&limit=${rows}&collapse=${interval}&column_index=${column}`;
+
+var results; 
+
+request(url, (error, response, body) => {
+  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode); 
+  results = JSON.parse(response.body);
+  console.log('results:', results.dataset.description); 
+}); 
+
+app.get('/', (req, res) => {
+  res.send(results);
+  res.end();
+})
+
+
+
+app.listen(3000, () => {
+  console.log(chalk.green('listening on port 3000')); 
+})
+
+
+
+
