07d41a4c538b1fc3a0897591b81dcf78f54dbef3
Alistair Willis <alistairrwillis@gmail.com>
2017-07-03 10:25:08 +0900

    change insert to update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 https://www.freecodecamp.com/challenges/chart-the-stock-market   
 https://docs.quandl.com/docs/parameters-2 
+
+#### To Do  
+
--- server/api/index.js
+++ server/api/index.js
@@ -1,36 +1,31 @@
 const request = require('request');
-const { connection } = require('../../mysql');
 const mysql = require('mysql');
-
+const { connection } = require('../../mysql');
 const { symbols } = require('../../mysql/process');
+const { api_key } = require('../config');
 
-const key = require('../config').api_key;
-let database_code = 'LSE';
-let dataset_code;
-let rows = 10; 
+let dataset_code = '';
 let interval = ['none', 'daily', 'weekly'][1];
-let column = 1; 
 
-
-var count = 0;
+var count = 15;
 
   var singleRequestID = setInterval(function() {
     dataset_code = symbols[count];
     count++;
-    if (count > 5) {
+    if (count > 18) {
       clearInterval(singleRequestID);
     }
     console.log(dataset_code);
 
-    let url = `https://www.quandl.com/api/v3/datasets/${database_code}/${dataset_code}.json?api_key=${key}&limit=&collapse=${interval}&column_index=${1}`;
+    let url = `https://www.quandl.com/api/v3/datasets/LSE/${dataset_code}.json?api_key=${api_key}&collapse=${interval}&column_index=1`;
 
     console.log('request');
     request(url, (err, res, body) => {
       if (err) {
         console.log('error:', error);
       } else { 
-        var sql = "INSERT INTO time_series_data (symbol, data_string) VALUES (?, ?)";
-        var inserts = [dataset_code, body];
+        var sql = "INSERT INTO time_series_data (symbol, data_string, updated) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE data_string=?, updated=?";
+        var inserts = [dataset_code, body, 'false', body, 'true'];
         sql = mysql.format(sql, inserts);
         connection.query(sql, (error, results, fields) => {
           if (error) throw error;
