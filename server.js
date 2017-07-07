ecd5f6e5e3e3720ee4b03c50c5246fa758740411
Alistair Willis <alistairrwillis@gmail.com>
2017-07-07 20:58:10 +0900

    final commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.listen(port, () => {
     console.log('listening on port:', port);
 });
 
-// updateData();
+updateData();
 setInterval(function() {
     console.log('updating...');
     updateData();
